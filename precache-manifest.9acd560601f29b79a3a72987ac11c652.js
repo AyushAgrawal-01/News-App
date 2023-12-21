75589eb185ea15a28b8370bb505ab4a160665f47
AyushAgrawal-01 <84064133+AyushAgrawal-01@users.noreply.github.com>
2023-12-21 20:52:49 +0530

    Updates

---
+++ precache-manifest.9acd560601f29b79a3a72987ac11c652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b226d8bff48fba6b693fe53f450bf8f",
+    "url": "/News-App/index.html"
+  },
+  {
+    "revision": "3888ac853f99110374eb",
+    "url": "/News-App/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9e562e4f7220d7e56329",
+    "url": "/News-App/static/js/2.6deedb69.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/News-App/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "3888ac853f99110374eb",
+    "url": "/News-App/static/js/main.9c95f80f.chunk.js"
+  },
+  {
+    "revision": "430fd14868c47e7ddbbb",
+    "url": "/News-App/static/js/runtime~main.a0946136.js"
+  },
+  {
+    "revision": "cfa50175b5fb9407cd6451dcd56f0701",
+    "url": "/News-App/static/media/loading.cfa50175.gif"
+  }
+]);
